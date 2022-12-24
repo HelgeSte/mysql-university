376394090af95ba251cc155a14a0580a620201c8
Helge Stegemoen <helge@stegemoen.com>
2022-12-24 08:44:41 +0100

    Initial project

---
+++ src/main/java/com/example/mysqluniversity/MysqlUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqluniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mysqluniversity/MysqlUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqluniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
